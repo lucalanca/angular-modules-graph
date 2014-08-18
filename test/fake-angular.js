2702e7c0efab5e0f67cef9c940cdb7382b5eac1f
Joao Figueiredo <lucalanca2@hotmail.com>
2014-08-18 19:45:24 +0200

    Adds more tests-

--- test/fake-angular.js
+++ test/fake-angular.js
@@ -138,8 +138,8 @@ describe("angular", function() {
     it('property should contain all defined controllers with array definition', function() {
       angular
         .module('testModule1', [])
-        .controller('testController1', ['dep1', function (asd) {}])
-        .controller('testController2', ['dep2', 'dep3', function (asd) {}]);
+        .controller('testController1', ['dep1', function (dep1) {}])
+        .controller('testController2', ['dep2', 'dep3', function (dep1, dep2) {}]);
 
       angular.modules.should.have.a.lengthOf(1)
       angular.modules[0].should.have.property('name', 'testModule1')
@@ -182,5 +182,60 @@ describe("angular", function() {
       testModule1.controllers[1].deps[0].should.be.equal('dep2');
       testModule1.controllers[1].deps[1].should.be.equal('dep3');
     })
+  })
+
+  it('property should contain all defined controllers with variable array definition', function() {
+    var testController1 = ['dep1', function (dep1) {}],
+        testController2 = ['dep2', 'dep3', function (dep2, dep3) {}]
+        ;
+
+    angular
+      .module('testModule1', [])
+      .controller('testController1', testController1)
+      .controller('testController2', testController2);
+
+    angular.modules.should.have.a.lengthOf(1)
+    angular.modules[0].should.have.property('name', 'testModule1')
+
+    var testModule1 = angular.modulesMap['testModule1'];
+    testModule1.controllers.should.have.a.lengthOf(2);
+    testModule1.controllers[0].should.have.property('name', 'testController1');
+    testModule1.controllers[1].should.have.property('name', 'testController2');
+
+    testModule1.controllers[0].deps.should.be.a.Array;
+    testModule1.controllers[0].deps.should.have.a.lengthOf(1);
+    testModule1.controllers[0].deps[0].should.be.equal('dep1')
+
+    testModule1.controllers[1].deps.should.be.a.Array;
+    testModule1.controllers[1].deps.should.have.a.lengthOf(2);
+    testModule1.controllers[1].deps[0].should.be.equal('dep2');
+    testModule1.controllers[1].deps[1].should.be.equal('dep3');
+  })
+
+  it('property should contain all defined controllers with variable function definition', function() {
+    function testController1 (dep1) {};
+    function testController2 (dep2, dep3) {};
+
+    angular
+      .module('testModule1', [])
+      .controller('testController1', testController1)
+      .controller('testController2', testController2);
+
+    angular.modules.should.have.a.lengthOf(1)
+    angular.modules[0].should.have.property('name', 'testModule1')
+
+    var testModule1 = angular.modulesMap['testModule1'];
+    testModule1.controllers.should.have.a.lengthOf(2);
+    testModule1.controllers[0].should.have.property('name', 'testController1');
+    testModule1.controllers[1].should.have.property('name', 'testController2');
+
+    testModule1.controllers[0].deps.should.be.a.Array;
+    testModule1.controllers[0].deps.should.have.a.lengthOf(1);
+    testModule1.controllers[0].deps[0].should.be.equal('dep1')
+
+    testModule1.controllers[1].deps.should.be.a.Array;
+    testModule1.controllers[1].deps.should.have.a.lengthOf(2);
+    testModule1.controllers[1].deps[0].should.be.equal('dep2');
+    testModule1.controllers[1].deps[1].should.be.equal('dep3');
   })
 })
